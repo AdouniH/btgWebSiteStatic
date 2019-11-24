cd6eaf73c06a38d9cb93508c0c5e780a1d0a12d4
AdouniH <houssemadouni11@gmail.com>
2019-11-24 23:22:55 +0100

    save changes

---
+++ build/precache-manifest.04c61a645bd9859e4ffc0617b4fe4302.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba171248e1120af9aa27b6bce0b64e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "390ef40f630a1bbc7eb0",
+    "url": "/static/css/main.b2c5583e.chunk.css"
+  },
+  {
+    "revision": "9c54fa05e5a7d0b14a1a",
+    "url": "/static/js/2.f5c71a44.chunk.js"
+  },
+  {
+    "revision": "390ef40f630a1bbc7eb0",
+    "url": "/static/js/main.9edfc69e.chunk.js"
+  },
+  {
+    "revision": "f356a8f5d66280e97748",
+    "url": "/static/js/runtime-main.2e2458a8.js"
+  },
+  {
+    "revision": "faf89b670553eaf2bb98b79e27372c01",
+    "url": "/static/media/comp.faf89b67.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "798f484923b67b2c12067cb3a56b76b6",
+    "url": "/static/media/logo.798f4849.png"
+  },
+  {
+    "revision": "565542734a6e6f9319edb888e5f7bf71",
+    "url": "/static/media/slide.56554273.png"
+  },
+  {
+    "revision": "02c7047a23615e4e630bb84d1a015827",
+    "url": "/static/media/test.02c7047a.jpg"
+  }
+]);
